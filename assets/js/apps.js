1ec706ec5e3d620f2ff9376b00989b9719b6f9e8
Rhonda Goolsby <rlgoolsby@gmail.com>
2025-04-14 22:45:34 -0400

    tab links now working

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,3 +1,22 @@
+$(window).scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+
+    // Show/hide menu on scroll
+    //if (scrollDistance >= 850) {
+    //		$('nav').fadeIn("fast");
+    //} else {
+    //		$('nav').fadeOut("fast");
+    //}
+
+    // Assign active class to nav links while scrolling
+    $('.page-section').each(function (i) {
+        if ($(this).position().top <= scrollDistance) {
+            $('.navigation link a.active').removeClass('active');
+            $('.navigation link a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
+
 var landing = angular.module("landing", ["ngRoute"]).run(["$rootScope", "$anchorScroll", function ($rootScope, $anchorScroll) {
   $rootScope.$on("$locationChangeSuccess", function () {
     $anchorScroll();
