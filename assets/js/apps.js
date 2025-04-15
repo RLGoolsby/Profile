19bb60236634795842077ddac405952e378d4a5e
Rhonda Goolsby <rlgoolsby@gmail.com>
2025-04-14 23:27:31 -0400

    total rehaul

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,47 +1,30 @@
-$(window).scroll(function () {
-    var scrollDistance = $(window).scrollTop();
 
-    // Show/hide menu on scroll
-    //if (scrollDistance >= 850) {
-    //		$('nav').fadeIn("fast");
-    //} else {
-    //		$('nav').fadeOut("fast");
-    //}
 
-    // Assign active class to nav links while scrolling
-    $('.page-section').each(function (i) {
-        if ($(this).position().top <= scrollDistance) {
-            $('.navigation link a.active').removeClass('active');
-            $('.navigation link a').eq(i).addClass('active');
-        }
-    });
-}).scroll();
+// var landing = angular.module("landing", ["ngRoute"]).run(["$rootScope", "$anchorScroll", function ($rootScope, $anchorScroll) {
+//   $rootScope.$on("$locationChangeSuccess", function () {
+//     $anchorScroll();
+//   });
+// }]);;
 
-var landing = angular.module("landing", ["ngRoute"]).run(["$rootScope", "$anchorScroll", function ($rootScope, $anchorScroll) {
-  $rootScope.$on("$locationChangeSuccess", function () {
-    $anchorScroll();
-  });
-}]);;
+// console.log("working");
+// landing.config(function ($routeProvider) {
+//   console.log("and now");
+//   $routeProvider
 
-console.log("working");
-landing.config(function ($routeProvider) {
-  console.log("and now");
-  $routeProvider
+//     .when("/bio", {
+//       templateUrl: "template/views/bio.html"
 
-    .when("/bio", {
-      templateUrl: "template/views/bio.html"
+//     })
+//     .when("/projects", {
+//       templateUrl: "template/views/projects.html"
 
-    })
-    .when("/projects", {
-      templateUrl: "template/views/projects.html"
+//     })
 
-    })
+//     .otherwise({
+//       redirectTo: "/home"
 
-    .otherwise({
-      redirectTo: "/home"
+//     });
 
-    });
-
-});
-console.log("and again");
+// });
+// console.log("and again");
 
