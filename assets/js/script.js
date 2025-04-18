5acad01adf6d3b99afb76057f6a882d74d798a22
Rhonda Goolsby <rlgoolsby@gmail.com>
2025-04-18 15:28:34 -0400

    lots of edits to all languages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,10 @@
-// $(window).on("scroll", function () {
-//   var $header = $("header");
+$(window).on("scroll", function () {
+  var $header = $("header");
 
-//   if ($(this).scrollTop() > 0) {
-//     $header.addClass("sticky");
-//   } else {
-//     $header.removeClass("sticky");
-//   }
-// });
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top', 
-    offset: 50
-});  
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-// The Smooth Scroll Effect
-$(document).on('click', 'a[href^="#"]', function(event){
-  event.preventDefault();
-  if($.attr(this, 'href')!="#"){
-    $('html, body').animate({
-       scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 800, 'swing');    
+  if ($(this).scrollTop() > 0) {
+    $header.addClass("sticky");
+  } else {
+    $header.removeClass("sticky");
   }
 });
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+
